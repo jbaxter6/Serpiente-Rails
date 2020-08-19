6fb6e619c19c737dab607d6961019f7e5fb3e6fa
jbaxter6 <jbaxter6@radford.edu>
2020-08-19 19:09:50 -0400

    validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     has_many :game_states
     has_many :records
+    validates :username, uniqueness: true
+    validates :username, length: { in: 5..10 }
+    validates :password, length: { in: 6..20 }
 
     def high_score
       return self.records.all.order("score DESC").first
